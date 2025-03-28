9db0eaba7545841c65796edc0cafc9897b82b465
stri0101 <1422281531@qq.com>
2025-03-28 21:17:56 +0800

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetson/Desktop/ai_control-FULL-3.0/src
+# Build directory: /home/jetson/Desktop/ai_control-FULL-3.0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("chassis_control")
+subdirs("opencv_detection")
