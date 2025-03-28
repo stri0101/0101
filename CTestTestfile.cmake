70d730e9df3950b41a5b5e3152d19c2c1bb7f094
stri0101 <1422281531@qq.com>
2025-03-28 21:24:02 +0800

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jetson/Desktop/ai_control-FULL-3.0/src
-# Build directory: /home/jetson/Desktop/ai_control-FULL-3.0/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("chassis_control")
-subdirs("opencv_detection")
